f0c5aa99d2ee4f008b49a400b785b94568b884a6
Nerdy-Thanos <47348592+Nerdy-Thanos@users.noreply.github.com>
2022-08-02 14:40:42 +0100

    Update gen_img.py

--- DCGan/gen_img.py
+++ DCGan/gen_img.py
@@ -13,5 +13,5 @@ fixed_noise = torch.randn(128, 128, 1, 1, device=device)
 with torch.no_grad():
     fake = cpkt(fixed_noise).detach().cpu()
 
-save_image(fake,"gen.jpg")
+save_image(fake[0],"gen.jpg")
 
