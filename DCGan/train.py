66156ade72794680522306f12ad182cbd7b5099b
Nerdy-Thanos <47348592+Nerdy-Thanos@users.noreply.github.com>
2022-08-02 15:40:19 +0100

    Update train.py

--- DCGan/train.py
+++ DCGan/train.py
@@ -171,7 +171,7 @@ for epoch in range(num_epochs):
         if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
             with torch.no_grad():
                 fake = netG(fixed_noise).detach().cpu()
-            img_list.append(fake)
+            img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
         iters += 1
 
@@ -195,6 +195,13 @@ plt.xlabel("iterations")
 plt.ylabel("Loss")
 plt.legend()
 plt.show()
+
+fig = plt.figure(figsize=(8,8))
+plt.axis("off")
+ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+
+HTML(ani.to_jshtml())
 
 # Grab a batch of real images from the dataloader
 real_batch = next(iter(dataloader))
