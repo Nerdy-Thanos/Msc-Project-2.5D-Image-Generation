e21930b2743842ffb63f20806fb9c5e7ec0df160
Nerdy-Thanos <47348592+Nerdy-Thanos@users.noreply.github.com>
2022-08-02 14:49:56 +0100

    Update gen_img.py

--- DCGan/gen_img.py
+++ DCGan/gen_img.py
@@ -7,11 +7,12 @@ from torchvision.utils import save_image
 device = torch.device("mps" if (torch.has_mps) else "cpu")
 
 cpkt = torch.load("DCGan/ckpt/trained_gen.pt")
+cpkt.to(device)
 print(cpkt.eval())
 
 fixed_noise = torch.randn(128, 128, 1, 1, device=device)
 with torch.no_grad():
-    fake = cpkt(fixed_noise).detach().cpu()
-
+    fake = cpkt(fixed_noise)
+fake.detach().cpu()
 save_image(fake[0],"gen.jpg")
 
